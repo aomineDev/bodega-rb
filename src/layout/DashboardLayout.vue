ce06e436d5ee8b2c78137d551bd2f0c8e9d5a8e9
aomine <aomine1745@gmail.com>
2025-10-23 02:08:27 -0500

    dashboard avance

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -1,27 +1,142 @@
-<script setup>
-import { RouterView, RouterLink } from 'vue-router'
-import { useUserStore } from '@/stores/user'
-
-const { user } = useUserStore()
-</script>
-
 <template>
-  <v-card>
-    <v-layout>
-      <v-app-bar :elevation="2">
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
+  <v-layout>
+    <v-navigation-drawer
+      v-model="drawer"
+      :rail="rail"
+      @click="rail = false"
+      :temporary="mobile"
+      :permanent="!mobile"
+    >
+      <v-list>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" :title="user">
+          <template #append>
+            <v-btn icon="mdi-chevron-left" variant="text" @click.stop="rail = !rail"></v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
 
-        <v-app-bar-title>Application Bar</v-app-bar-title>
+      <v-list density="compact" nav>
+        <v-list-item
+          title="Dashboard"
+          prepend-icon="mdi-view-dashboard"
+          value="dashboard"
+          active-color="primary"
+          to="/home"
+        ></v-list-item>
 
-        <template v-slot:append>
-          <RouterLink to="/home">Home</RouterLink> | <RouterLink to="/about">about</RouterLink> |
-          {{ user }}
-          <RouterLink to="/login">Log out</RouterLink>
-        </template>
-      </v-app-bar>
-      <RouterView />
-    </v-layout>
-  </v-card>
+        <v-list-item
+          v-for="item in cajaitems"
+          :key="item.value"
+          :title="item.title"
+          :prepend-icon="item.icon"
+          :value="item.value"
+          :to="item.to"
+          color="primary"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+        <v-list-subheader>Inventario</v-list-subheader>
+
+        <v-list-item
+          v-for="item in inventarioItems"
+          :key="item.value"
+          :title="item.title"
+          :prepend-icon="item.icon"
+          :value="item.value"
+          :to="item.to"
+          active-color="primary"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+        <v-list-subheader>Almacen</v-list-subheader>
+
+        <v-list-item
+          title="Ingreso de productos"
+          prepend-icon="mdi-view-dashboard"
+          value="ingrteso"
+          active-color="primary"
+          to="/path"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+        <v-list-subheader>Gestion</v-list-subheader>
+
+        <v-list-item
+          v-for="item in gestionItems"
+          :key="item.value"
+          :title="item.title"
+          :prepend-icon="item.icon"
+          :value="item.value"
+          :to="item.to"
+          active-color="primary"
+        ></v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <v-btn color="error" @click="logout" block variant="text">
+          <v-icon start>mdi-logout</v-icon>
+          Cerrar Sesión
+        </v-btn>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar :elevation="2">
+      <template #prepend v-if="mobile">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>
+        <v-breadcrumbs :items="['Embutidos RB', 'Dashboard']"></v-breadcrumbs
+      ></v-app-bar-title>
+
+      <template #append>
+        <v-btn icon="mdi-heart"></v-btn>
+
+        <v-btn icon="mdi-magnify"></v-btn>
+
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <RouterView />
+      </v-container>
+    </v-main>
+  </v-layout>
 </template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+import { ref } from 'vue'
+import router from '@/router'
+import { useDisplay } from 'vuetify'
+
+const { user, setUser } = useUserStore()
+const { mobile } = useDisplay()
+const drawer = ref(true)
+const rail = ref(true)
+
+const cajaitems = [
+  { title: 'Ventas', icon: 'mdi-view-dashboard', value: 'dashboard', to: '/path' },
+  { title: 'Clientes', icon: 'mdi-account', value: 'clientes', to: '/about' },
+  { title: 'Boletas / Facturas', icon: 'mdi-cog', value: 'boletas', to: '/path' },
+]
+const inventarioItems = [
+  { title: 'Tima de inventario', icon: 'mdi-account', value: 'inventario', to: '/path' },
+  { title: 'Auditoria', icon: 'mdi-account', value: 'inventario', to: '/path' },
+]
+
+const gestionItems = [
+  { title: 'Proveedores', icon: 'mdi-account', value: 'proveedores', to: '/path' },
+  { title: 'Empleados', icon: 'mdi-account', value: 'empleados', to: '/path' },
+  { title: 'Productos', icon: 'mdi-account', value: 'productos', to: '/path' },
+  { title: 'Categorias', icon: 'mdi-account', value: 'categorias', to: '/path' },
+]
+
+const logout = () => {
+  setUser(null)
+  router.push('/login')
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -13,6 +15,9 @@ import router from './router'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
 })
 
 const app = createApp(App)
