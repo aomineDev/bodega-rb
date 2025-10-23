57dff860abc5c6632a3d2e7fe0481357de1f9858
aomine <aomine1745@gmail.com>
2025-10-23 17:18:48 -0500

    login input

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,9 +2,13 @@
 import router from '@/router'
 import { ref } from 'vue'
 import { useUserStore } from '../stores/user'
+
 const username = ref('')
 
-let { setUser } = useUserStore()
+const userStore = useUserStore()
+
+const { setUser } = userStore
+
 function handleLogin() {
   setUser(username.value)
 
@@ -15,8 +19,7 @@ function handleLogin() {
 <template>
   <div>
     <h1>Login</h1>
-    <v-text-field label="Label" v-model="username"></v-text-field>
-    <p>{{ username }}</p>
+    <v-text-field label="Label" v-model="username" variant="underlined"></v-text-field>
     <v-btn color="success" @click="handleLogin">login</v-btn>
   </div>
 </template>
